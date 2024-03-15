f94876386bc18e7126fbef28c3e584a82d1d02f5
zalesiu <zalewmat31@gmail.com>
2024-03-15 21:27:20 +0100

    feat: add route /api/users with get and post methods

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
+
+const users = [];
+const exercises = [];
 
 app.use(cors());
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+app
+  .route("/api/users")
+  .get((req, res) => {
+    res.json(users);
+  })
+  .post((req, res) => {
+    const { username } = req.body;
+    const newUser = { _id: uuidv4(), username: username };
+    res.json(newUser);
+    users.push(newUser);
+  });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
