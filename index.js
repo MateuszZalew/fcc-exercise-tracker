26d96c46393b6afaf9a0bfcdd78076c0d1a69621
zalesiu <zalewmat31@gmail.com>
2024-03-15 21:29:12 +0100

    feat: create GET route to get user's logs, add filter option with query variables

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+const formatDate = (input) => {
+  const date = new Date(input);
+  const year = date.toLocaleString("default", { year: "numeric" });
+  const month = date.toLocaleString("default", { month: "2-digit" });
+  const day = date.toLocaleString("default", { day: "2-digit" });
+  const formattedDate = `${year}-${month}-${day}`;
+  return formattedDate;
+};
+
 app
   .route("/api/users")
   .get((req, res) => {
@@ -43,6 +52,30 @@ app.post("/api/users/:id/exercises", (req, res) => {
   };
   res.json(newExercise);
   exercises.push(newExercise);
+});
+
+app.get("/api/users/:id/logs?", (req, res) => {
+  const { id } = req.params;
+  const { from, to, limit } = req.query;
+  const user = users.find((user) => user._id === id);
+  let userExercises = exercises.filter((exercise) => exercise._id === user._id);
+  if (from)
+    userExercises = userExercises.filter(
+      ({ date }) => formatDate(date) >= from
+    );
+  if (to)
+    userExercises = userExercises.filter(({ date }) => formatDate(date) <= to);
+  if (limit) userExercises = userExercises.slice(0, limit);
+  userExercises = userExercises.map((userExercise) => {
+    const { description, duration, date } = userExercise;
+    return {
+      description,
+      duration,
+      date,
+    };
+  });
+  const log = { ...user, count: userExercises.length, log: userExercises };
+  res.json(log);
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
